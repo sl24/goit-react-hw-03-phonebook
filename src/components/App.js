52dbbe4c2043d80527ffd85349a02f6e82dbdbbf
sl24 <slvitalik24@gmail.com>
2020-12-11 11:18:58 +0200

    Add hw-3

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,20 @@ import { Container, MainTitle, ContactTitle } from './AppStyles';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  };
+
+  componentDidMount = () => {
+    const userContacts = JSON.parse(localStorage.getItem('userContacts'));
+    if (userContacts) this.setState({ contacts: userContacts });
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('userContacts', JSON.stringify(contacts));
+    }
   };
 
   handleAddContact = newContact =>
